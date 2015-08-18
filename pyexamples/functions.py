1c3f5a07a4eb5d535d3b67dd5bfc62ca32cb84db
Caio Herrera <caio-herrera05@hotmail.com>
2015-08-18 11:20:21 -0300

    Modified return of function

--- pyexamples/functions.py
+++ pyexamples/functions.py
@@ -3,9 +3,9 @@ pedidos = []
 
 def adiciona_pedido(nome, sabor):
     pedido = {'nome': nome, 'sabor': sabor}
-    pedidos.append(pedido)
+    return pedido
 
-adiciona_pedido('Mario', 'pepperoni')
-adiciona_pedido('Marco', 'bacon')
+pedidos.append(adiciona_pedido('Mario', 'pepperoni'))
+pedidos.append(adiciona_pedido('Marco', 'bacon'))
 for pedido in pedidos:
     print('Nome: {0}\nSabor: {1}\n'.format(pedido['nome'], pedido['sabor']))
