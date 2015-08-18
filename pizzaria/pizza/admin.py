656100d6baec6fd4c4641a8c79bf815094106264
Caio Herrera <caio-herrera05@hotmail.com>
2015-08-18 15:56:07 -0300

    Added picture of pizzas

--- pizzaria/pizza/admin.py
+++ pizzaria/pizza/admin.py
@@ -3,7 +3,7 @@ from .models import Pizza
 
 class PizzaAdmin(admin.ModelAdmin):
     search_fields = ['name', 'ingredients']
-    list_display = ('name', 'price', 'ingredients')
+    list_display = ('name', 'price', 'ingredients', 'picture')
 
 # Register your models here.
 admin.site.register(Pizza, PizzaAdmin)
