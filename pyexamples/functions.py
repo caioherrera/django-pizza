29d42cb0f49bbc5713c049b0d361ba2593794271
Caio Herrera <caio-herrera05@hotmail.com>
2015-08-18 11:11:04 -0300

    Added functions file

---
+++ pyexamples/functions.py
@@ -0,0 +1,10 @@
+pedidos = []
+
+def adiciona_pedido(nome, sabor):
+    pedido = {'nome': nome, 'sabor': sabor}
+    pedidos.append(pedido)
+
+adiciona_pedido('Mario', 'pepperoni')
+adiciona_pedido('Marco', 'bacon')
+for pedido in pedidos:
+    print('Nome: {0}\nSabor: {1}\n'.format(pedido['nome'], pedido['sabor']))
