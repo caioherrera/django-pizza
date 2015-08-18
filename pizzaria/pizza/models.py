db6e5497d28a94df9eef46847a300456fccf7dea
Caio Herrera <caio-herrera05@hotmail.com>
2015-08-18 15:28:54 -0300

    Added pizza admin

---
+++ pizzaria/pizza/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Pizza(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    ingredients = models.TextField()
+
+    def __unicode__(self):
+        return u'Pizza: {}'.format(self.name)
+
+    def __repr__(self):
+        return unicode(self)
