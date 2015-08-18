f4d2ccfe25dae7f19baa195bf101eed4e240f448
Caio Herrera <caio-herrera05@hotmail.com>
2015-08-18 16:55:24 -0300

    Listed pizza names in the menu

--- pizzaria/pizza/views.py
+++ pizzaria/pizza/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Pizza
+
 # Create your views here.
 def menu(request):
-    return HttpResponse('Hello World!')
+    pizzas = Pizza.objects.all()
+    result = ''
+    for pizza in pizzas:
+        result += pizza.name
+        result += '<br>'
+    return HttpResponse(result)
+
     
