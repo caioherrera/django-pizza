a4763cf1b0cbcd1267ace710eff71a49bb7496d8
Caio Herrera <caio-herrera05@hotmail.com>
2015-08-18 15:39:54 -0300

    Added pizza admin

--- pizzaria/pizza/admin.py
+++ pizzaria/pizza/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Pizza
 
+class PizzaAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'ingredients']
+    list_display = ('name', 'price', 'ingredients')
+
 # Register your models here.
-admin.site.register(Pizza)
+admin.site.register(Pizza, PizzaAdmin)
